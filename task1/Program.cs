99756410a9c384646dbe9ae8c6b0e592d412ef48
Tolege Sultan <sultantulegenov91@gmail.com>
2023-04-13 21:09:55 +0300

    wrote code for task 1

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите цикл, который принимает на вход два числа
+// (A и B) и возводит число A в натуральную степень B.
+
+int ReadInt(string massage)
+{
+    System.Console.Write($"{massage} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int a = ReadInt("Введите первое число");
+int b = ReadInt("Введите второе число");
+int size = 1;
+int result = 1;
+while (size <= b)
+{
+    result = result * a;
+    size++;
+}
+Console.WriteLine(result);
