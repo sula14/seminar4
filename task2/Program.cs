96bd83f50d87abf687f699d0dc899ec99e006e62
Tolege Sultan <sultantulegenov91@gmail.com>
2023-04-15 18:02:23 +0300

    wrote code for task 2

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int ReadInt(string massage)
+{
+    System.Console.Write($"{massage} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = ReadInt("Введите число");
+int res = 0;
+while (num > 0)
+{
+    res = res + num % 10;
+    num = num / 10;
+}
+
+Console.WriteLine($"Сумма числа равна {res}");
